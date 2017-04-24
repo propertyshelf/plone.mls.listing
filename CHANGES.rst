9da8443d8aa879bb0fedd76c9b5df0ada38dc8b8
Thomas Massmann <thomas@propertyshelf.com>
2017-04-24 09:54:42 +0200

    Preparing release 1.11

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.11 (unreleased)
+1.11 (2017-04-24)
 -----------------
 
 - Add Plone 5 compatibility.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
     setup,
 )
 
-version = '1.11.dev0'
+version = '1.11'
 description = 'Plone support for MLS Listings.'
 long_description = ('\n'.join([
     open('README.rst').read(),
