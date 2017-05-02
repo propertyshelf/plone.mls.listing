52686ae3725e1e31dde0532200b30e625f4c0e65
Thomas Massmann <thomas@propertyshelf.com>
2017-05-02 14:31:59 +0200

    Move i18n message id into a constant, will be used multiple times.

--- src/plone/mls/listing/browser/adapter.py
+++ src/plone/mls/listing/browser/adapter.py
@@ -8,17 +8,32 @@ from z3c.form.widget import StaticWidgetAttribute
 from plone.mls.listing import browser
 from plone.mls.listing.i18n import _
 
+LABEL_ALL = _(u'All')
+
+
 PleaseSelectLCC = StaticWidgetAttribute(
-    _(u'All'), view=browser.listing_collection.ListingCollectionConfiguration)
+    LABEL_ALL,
+    view=browser.listing_collection.ListingCollectionConfiguration,
+)
 PleaseSelectLSF = StaticWidgetAttribute(
-    _(u'All'), view=browser.listing_search.ListingSearchForm)
+    LABEL_ALL,
+    view=browser.listing_search.ListingSearchForm,
+)
 PleaseSelectRLC = StaticWidgetAttribute(
-    _(u'All'), view=browser.recent_listings.RecentListingsConfiguration)
+    LABEL_ALL,
+    view=browser.recent_listings.RecentListingsConfiguration,
+)
 
 
 PleaseSelectState = StaticWidgetAttribute(
-    _(u'All'), field=browser.listing_search.IListingSearchForm['location_state'])
+    LABEL_ALL,
+    field=browser.listing_search.IListingSearchForm['location_state'],
+)
 PleaseSelectCounty = StaticWidgetAttribute(
-    _(u'All'), field=browser.listing_search.IListingSearchForm['location_county'])
+    LABEL_ALL,
+    field=browser.listing_search.IListingSearchForm['location_county'],
+)
 PleaseSelectDistrict = StaticWidgetAttribute(
-    _(u'All'), field=browser.listing_search.IListingSearchForm['location_district'])
+    LABEL_ALL,
+    field=browser.listing_search.IListingSearchForm['location_district'],
+)
