c6240aba755667454dd49b169d77b6151ee160e5
Thomas Massmann <thomas@propertyshelf.com>
2018-05-30 17:06:25 +0200

    Preparing release 1.18

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
     setup,
 )
 
-version = '1.18.dev0'
+version = '1.18'
 description = 'Plone support for MLS Listings.'
 long_description = ('\n'.join([
     open('README.rst').read(),
