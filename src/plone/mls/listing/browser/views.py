98de2f8596ab7f64caefc7120e72328116e319c3
Zach Cashero <zach@propertyshelf.com>
2020-01-06 01:11:17 -0500

    Fix error due to tile sizes being 512px

--- src/plone/mls/listing/browser/views.py
+++ src/plone/mls/listing/browser/views.py
@@ -103,6 +103,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{{id}}/tiles/{{z}}/{{x}}/{{y}}?acc
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     maxZoom: 18,
     tileSize: 512,
+    zoomOffset: -1,
     id: 'mapbox/streets-v11'
 }}).addTo(map);
 """
