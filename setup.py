2e3286d76a9ed78e534afc1bb1b14ec1eb8da0d4
Zach Cashero <zach@propertyshelf.com>
2023-08-04 02:05:57 -0500

    Back to development: 1.22

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import (
     setup,
 )
 
-version = '1.21'
+version = '1.22.dev0'
 description = 'Plone support for MLS Listings.'
 long_description = ('\n'.join([
     open('README.rst').read(),
